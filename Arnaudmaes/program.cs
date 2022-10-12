33e9872f98cea179d7089a4c3bc82f32ff76e926
maestrict <arnaud.maes.info@gmail.com>
2022-10-12 15:18:51 +0200

    penduV1

---
+++ Arnaudmaes/program.cs
@@ -0,0 +1,81 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// class program {
+
+//     static char[] verif(char[] mot){
+//         int count =0;
+    
+//         string lettre = Console.ReadLine();
+//         System.Console.WriteLine(lettre);
+//         for (int i = 0; i < mot.Length; i++) { 
+//             if(mot[i] == lettre[0]){
+//                 System.Console.WriteLine("c'est bon");
+//                 count ++;
+//             }else{
+//                 System.Console.WriteLine("c'est pas bon");
+//             }
+//         }
+//         if(count>0){
+//             lettreTrouve[lettreTrouve.Length-1] = lettre[0];
+//         }else{
+//             lettrerater[lettrerater.Length-1]=lettre[0];
+//         }
+//         return mot;
+//     }
+//     static char[] construct(string mot){
+//         char[] array = new char[mot.Length];
+
+//         for (int i = 0; i < mot.Length; i++) { 
+//                 array[i] = mot[i];
+//             }
+//         return array;
+//     }
+  
+//     // Main Method
+//     static public void Main(String[] args){
+//         public static int life = 5;
+//         char[] lettreTrouve = new char[100];
+//         char[] lettrerater = new char[100];
+//         bool test = false;
+//         string mot = "test";
+        
+        
+//         //arrayMot[arrayMot.Length-1] = "test";
+//         char[] arrayMot = construct(mot);
+        
+
+//     }
+
+
+// }
+
+using arnaudmaes.Structures;
+System.Console.WriteLine("Quelle mot?");
+
+string mot = Console.ReadLine();
+char[] array = new char[mot.Length];
+for (int i = 0; i < mot.Length; i++) { 
+         array[i] = mot[i];
+         }
+int tailleMot=mot.Length;
+pendu pendu = new pendu(array, tailleMot);
+System.Console.WriteLine(tailleMot);
+while(pendu.generalCount != 0){
+    System.Console.WriteLine("Quelle lettre?");
+    string lettre = Console.ReadLine();
+    System.Console.WriteLine(pendu.verif(lettre));
+    System.Console.WriteLine(pendu.mot);
+    System.Console.WriteLine(pendu.generalCount);
+    if(pendu.life==0){
+        System.Console.WriteLine("c'est perdu");
+        break;
+    }
+}
+if(pendu.generalCount==0){
+    System.Console.WriteLine(("félicitation vous avez trouvé le mot!"));
+}
+
+
+
---
+++ Arnaudmaes/structure/pendu.cs
@@ -0,0 +1,60 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arnaudmaes.Structures
+{
+    internal struct pendu 
+    {
+        private int _life = 5;
+        public int life { get {return _life;} set { _life = value; }}
+
+        private int _generalCount;
+        public int generalCount { get {return _generalCount;} set { _generalCount = value; }}
+
+
+        private char[] _lettreTrouve = new char[100];
+
+        public char[] lettreTrouve { get {return _lettreTrouve;}}
+        private char[] _lettrerater = new char[100];
+        public char[] lettrerater { get {return _lettrerater;}}
+        private char[] _mot;
+        public char[] mot { get {return _mot;}}
+
+
+        public pendu(char[] mot, int generalCount):this()
+        {
+            _mot = mot;
+            _generalCount = generalCount;
+        }
+
+        /// <summary>
+        /// ajoute des km la voiture
+        /// </summary>
+        public string verif(string lettre)
+        {
+         int count = 0;
+         for(int i=0;i<mot.Length;i++){
+            if(mot[i]==lettre[0]){
+                count++;
+                generalCount--;
+            }
+         }
+         if(count>0){
+            System.Console.WriteLine("félicitation");
+            lettreTrouve[lettreTrouve.Length-1]= lettre[0];
+         }else{
+            life--;
+            lettrerater[lettrerater.Length-1] = lettre[0];
+         }
+         return lettre;   
+        }
+
+        
+
+
+    }
+}
